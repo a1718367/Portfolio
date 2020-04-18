48dcbd668483bd2855e1d3315c8ab03b59a9a4bc
PzOng <pangszee.ong@gmail.com>
2020-04-19 00:56:20 +0930

    init

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+window.addEventListener("resize", function(){
+    let viewportwidth = window.innerWidth;
+    console.log(viewportwidth);
+
+    
+})
+
+$('#quiz').on('click',function(){
+    window.open('https://a1718367.github.io/quiz_generator/','_blank')
+})
+$('#golf').on('click',function(){
+    window.open('https://a1718367.github.io/GolfApp-v2/', '_blank')
+})
+$('#weather').on('click',function(){
+    window.open('https://a1718367.github.io/weatherApp/', '_blank')
+})
