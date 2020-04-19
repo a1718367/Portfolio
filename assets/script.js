a2cfdb44fa054270a8a3c8f64057b0577676933b
PzOng <pangszee.ong@gmail.com>
2020-04-19 23:44:04 +0930

    wip1

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,21 @@
 
+resize();
 window.addEventListener("resize", function(){
+
     let viewportwidth = window.innerWidth;
     console.log(viewportwidth);
 if(viewportwidth<=568){
     document.getElementById('banner').style.height="25rem";
-    document.getElementById('bannertext').style.paddingTop="5rem"
+    document.getElementById('bannertext').style.paddingTop="3rem";
+    document.getElementById('about').style.paddingTop="1rem";
+    document.getElementById('about').style.fontSize="20px";
+    document.getElementById('bannertext').style.textAlign="center";
 }else{
-    document.getElementById('banner').style.height="40rem";
-    document.getElementById('bannertext').style.paddingTop="15rem"
+    document.getElementById('banner').style.height="50rem";
+    document.getElementById('bannertext').style.paddingTop="8rem";
+    document.getElementById('about').style.paddingTop="15rem";
+    document.getElementById('about').style.fontSize="25px";
+    document.getElementById('bannertext').style.textAlign="right";
     
 }
     
@@ -22,3 +30,42 @@ $('#golf').on('click',function(){
 $('#weather').on('click',function(){
     window.open('https://a1718367.github.io/weatherApp/', '_blank')
 })
+
+window.addEventListener('scroll',function(){
+    let y = window.pageYOffset;
+    console.log(y);
+if(y<=200){
+    document.getElementById('h3').style.color="white";
+}else{
+    document.getElementById('h3').style.color="black"
+};
+if(y<=440){
+    document.getElementById('h2').style.color="white";
+}else{
+    document.getElementById('h2').style.color="black"
+};
+if(y<=660){
+    document.getElementById('h1').style.color="white";
+}else{
+    document.getElementById('h1').style.color="black"
+}
+})
+
+function resize(){
+let viewportwidth = window.innerWidth;
+    console.log(viewportwidth);
+if(viewportwidth<=568){
+    document.getElementById('banner').style.height="25rem";
+    document.getElementById('bannertext').style.paddingTop="3rem";
+    document.getElementById('about').style.paddingTop="1rem";
+    document.getElementById('about').style.fontSize="20px";
+    document.getElementById('bannertext').style.textAlign="center";
+}else{
+    document.getElementById('banner').style.height="50rem";
+    document.getElementById('bannertext').style.paddingTop="8rem";
+    document.getElementById('about').style.paddingTop="15rem";
+    document.getElementById('about').style.fontSize="25px";
+    document.getElementById('bannertext').style.textAlign="right";
+    
+}
+}
